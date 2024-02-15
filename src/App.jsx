7c18f81226e057442e3d6c9761aed00e4fa51f91
Mitchel Alejo <michalejo94@gmail.com>
2024-02-15 12:59:51 -0600

    Feat: Installed packages created Header file

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <Router>
+      <>
+        <Header />
+      </>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+  return (
+    <div>
+      <h1>test header</h1>
+    </div>
+  );
+}
+
+export default Header;
