70b7220b019affce62045e12016602a83598ae9c
Mitchel Alejo <michalejo94@gmail.com>
2024-02-21 14:59:59 -0600

    Feat:Experience Page styling

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -10,26 +10,15 @@ function Experience() {
       <div className="experiencePageContainer">
         <div className="educationContainer">
           <section>
-            <h1>Education</h1>
-            <ul>
-              <li>Dominican University</li>
-              <li>FullStack</li>
-            </ul>
+            <div className="educationText">
+              <h1>Education</h1>
+              <ul>
+                <li>Dominican University</li>
+                <li>FullStack</li>
+              </ul>
+            </div>
           </section>
-        </div>
-        <div className="skillsContainer">
           <section>
-            <h1>Skills</h1>
-            <p>
-              Yapper you are doing me a frighten he made many woofs very jealous
-              pupper vvv wow such tempt, heckin lotsa pats heck. Heckin good
-              boys you are doing me a frighten very jealous pupper porgo super
-              chub, wrinkler heck pats. Woofer heckin good boys and girls puggo
-              many pats, length boy. Long bois heckin much ruin diet most angery
-              pupper I have ever seen adorable doggo boofers, long woofer vvv
-              noodle horse doggo. Lotsa pats length boy smol borking doggo with
-              a long snoot for pats heckin angery woofer, wow such tempt shibe.
-            </p>
             <div className="resumeCard">
               <Card
                 onClick={() => {
@@ -50,6 +39,22 @@ function Experience() {
               </Card>
             </div>
           </section>
+        </div>
+        <div className="skillsContainer">
+          <div className="skillsText">
+            <h1>Skills</h1>
+
+            <p>
+              Yapper you are doing me a frighten he made many woofs very jealous
+              pupper vvv wow such tempt, heckin lotsa pats heck. Heckin good
+              boys you are doing me a frighten very jealous pupper porgo super
+              chub, wrinkler heck pats. Woofer heckin good boys and girls puggo
+              many pats, length boy. Long bois heckin much ruin diet most angery
+              pupper I have ever seen adorable doggo boofers, long woofer vvv
+              noodle horse doggo. Lotsa pats length boy smol borking doggo with
+              a long snoot for pats heckin angery woofer, wow such tempt shibe.
+            </p>
+          </div>
         </div>
       </div>
     </>
