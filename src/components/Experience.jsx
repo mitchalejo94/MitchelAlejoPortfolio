51969f0e0512c1b0662989cc935f26f65d59cc80
Mitchel Alejo <michalejo94@gmail.com>
2024-02-26 15:03:58 -0600

    Experience page media completed

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -12,36 +12,34 @@ function Experience() {
   return (
     <>
       <div className="experiencePageContainer">
-        <div className="experienceTextContainer">
+        <div className="skillsContainer">
           <section>
-            <div className="skillsContainer">
-              {" "}
-              <h1>Skills:</h1>
-              <p>
-                My skill set includes JavaScript, React, Redux, Node.js,
-                Express, PostgreSQL, mySql, Sequelize, Next.js, TailwindCSS,
-                HTML, CSS, and Git, I possess a solid grasp of both front-end
-                and back-end technologies crucial for crafting modern,
-                user-centric web applications.
-              </p>
-            </div>
-
-            <div className="educationContainer">
-              <h1>Education:</h1>
-              <div className="educationList">
-                <ul className="listItem">Dominican University</ul>
-                <ul className="listItem">
-                  •Bachelor of Arts, Criminology and Sociology
-                </ul>
-
-                <ul className="listItem">
-                  Fullstack Academy Web Development Bootcamp
-                </ul>
-                <ul className="listItem">•Software Engineering Certificate</ul>
-              </div>
-            </div>
+            {" "}
+            <h2>Skills:</h2>
+            <p>
+              My skill set includes JavaScript, React, Redux, Node.js, Express,
+              PostgreSQL, mySql, Sequelize, Next.js, TailwindCSS, HTML, CSS, and
+              Git, I possess a solid grasp of both front-end and back-end
+              technologies crucial for crafting modern, user-centric web
+              applications.
+            </p>
           </section>
         </div>
+        <div className="educationContainer">
+          <h2>Education:</h2>
+          <div className="educationList">
+            <h3 className="listItem EduTitle">Dominican University</h3>
+            <h3 className="listItem EduDescrip">
+              B.A- Criminology and Sociology
+            </h3>
+
+            <h3 className="listItem EduTitle">Fullstack Academy Bootcamp</h3>
+            <h3 className="listItem EduDescrip">
+              •Certificate in Web Development
+            </h3>
+          </div>
+        </div>
+
         <div className="resumeCardContainer">
           <Card
             ref={cardRef}
